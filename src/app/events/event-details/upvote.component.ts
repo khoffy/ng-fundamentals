53812d1c415c8194352c752b7b73ab117a9f2584
khoffy <khoffy.tipoh@gmail.com>
2020-08-19 21:28:04 +0000

    Project angular-funamentl on Pluralsight

---
+++ src/app/events/event-details/upvote.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: 'upvote',
+  template: `
+    <div class="votingWidgetContainer pointable" (click)="onClick()">
+      <div class="well votingWidget">
+        <div class="votingButton">
+          <i class="glyphicon glyphicon-heart"
+            [style.color]="iconColor"></i>
+        </div>
+        <div class="badge badge-inverse">
+          <div>{{count}}</div>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: ['./upvote.component.css']
+})
+
+export class UpvoteComponent {
+  @Input() count: number;
+  @Input() set voted(val) {
+    this.iconColor = val ? "red" : "white";
+  }
+  @Output() vote = new EventEmitter();
+  iconColor: string;
+
+  onClick() {
+    this.vote.emit({})
+  }
+}
